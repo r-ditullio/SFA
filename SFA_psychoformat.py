f2940b0855e39c4a8312a5ab2bb8905acce56159
Ron W DiTullio <ron.w.ditullio@gmail.com>
2019-09-23 19:46:32 -0400

    Just a quick back up

---
+++ SFA_psychoformat.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 10:42:18 2019
+
+SFA batches of stimuli
+
+Make a copy of SFA_streamlined_format and then try to set it up so this call that module, but can add additional stimuli.
+
+@author: ronwd
+"""
+
